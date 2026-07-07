4dcc06e8b805d211b5b2ce333004c4dd9fa8fd7e
davidsmurfy11111 <david@themckinnons.org>
2026-07-07 17:47:03 +1000

    Create PSNVersion.h

---
+++ PSNVersion.h
@@ -0,0 +1,12 @@
+#ifndef PSN_VERSION_H
+#define PSN_VERSION_H
+
+namespace psn
+{
+    constexpr uint8_t VersionMajor = 2;
+    constexpr uint8_t VersionMinor = 2;
+
+    constexpr const char LibraryVersion[] = "0.1.0";
+}
+
+#endif
