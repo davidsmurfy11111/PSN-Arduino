3710c569c9f37a97a9b98229d684ea300405795c
davidsmurfy11111 <david@themckinnons.org>
2026-07-07 18:25:21 +1000

    Update PSNPacketBuilder.h

--- src/PSNPacketBuilder.h
+++ src/PSNPacketBuilder.h
@@ -13,6 +13,48 @@ public:
 
 private:
     PSNWriter& m_writer;
+public:
+    bool beginInfoPacket();
+    bool beginDataPacket();
+
+private:
+    bool writeHeader();
+
+bool PSNPacketBuilder::writeHeader()
+{
+    if (!m_writer.beginChunk(psn::Header))
+        return false;
+
+    if (!m_writer.writeU64(psn::currentTimeMicros()))
+        return false;
+
+    return m_writer.endChunk();
+}
+bool PSNPacketBuilder::beginDataPacket()
+{
+    m_writer.reset();
+
+    if (!m_writer.beginChunk(psn::DataPacket, true))
+        return false;
+
+    if (!writeHeader())
+        return false;
+
+    return true;
+}
+bool PSNPacketBuilder::beginInfoPacket()
+{
+    m_writer.reset();
+
+    if (!m_writer.beginChunk(psn::InfoPacket, true))
+        return false;
+
+    if (!writeHeader())
+        return false;
+
+    return true;
+}
 };
 
 #endif
+
